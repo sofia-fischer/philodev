45a0ac344b61e6f9ce77fd417096adb75a1fdc2a
Sofia Christ <sofia.christ@tum.de>
2021-02-14 21:46:08 +0100

    fix deployment script

--- deployment/scripts/deploy.production.sh
+++ deployment/scripts/deploy.production.sh
@@ -4,8 +4,6 @@ cd /var/www/html/philodev/
 
 echo $HETZNER_SSH_PASSWORD | sudo -kS chown -R $HETZNER_SSH_USERNAME:$HETZNER_SSH_USERNAME /var/www/html/philodev
 
-php artisan migrate --force
-
 php artisan config:cache
 
 php artisan route:clear
