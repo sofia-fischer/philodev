e7ed26624d26feb3cd53a95b4b2a0acc41f21ad1
Sofia Christ <sofia.christ@tum.de>
2021-02-16 18:27:39 +0100

    fix deployment script

--- deployment/scripts/deploy.production.sh
+++ deployment/scripts/deploy.production.sh
@@ -2,7 +2,7 @@ echo $HETZNER_SSH_PASSWORD | sudo -kS cp -r dev/philodev/* /var/www/html/philode
 
 cd /var/www/html/philodev/
 
-echo $HETZNER_SSH_PASSWORD | sudo -kS chown -R $HETZNER_SSH_USERNAME:$HETZNER_SSH_USERNAME /var/www/html/philodev
+echo $HETZNER_SSH_PASSWORD | sudo -kS chown -R www-data:www-data /var/www/html/philodev
 
 php artisan config:cache
 
