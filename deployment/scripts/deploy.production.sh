ac5298a2ca0133b8d8aa41929e7b4a6ceaa63122
sofia <sofia@philodev.com>
2022-03-29 13:23:39 +0200

    🚚 Updates Packages and add codyo as old project

--- deployment/scripts/deploy.production.sh
+++ deployment/scripts/deploy.production.sh
@@ -1,11 +1,11 @@
 # delete files
 echo $HETZNER_SSH_PASSWORD | sudo -kS rm -rf /var/www/html/philodev/*
 
-# copy and replace .env file
-cp dev/philodev/.env.production dev/philodev/.env
-
 # copy files to /var/www/
 echo $HETZNER_SSH_PASSWORD | sudo -kS cp -r dev/philodev/* /var/www/html/philodev/
+
+# copy and replace .env file
+cp dev/philodev/.env.production /var/www/html/philodev/.env
 
 # fix permissions from sudo copy
 echo $HETZNER_SSH_PASSWORD | sudo -kS chown -R www-data:www-data /var/www/html/philodev
