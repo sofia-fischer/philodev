ca3ee0cfa74b9abbe1e0ed054d88f6f95b06cbce
sofia <sofia@philodev.com>
2022-04-28 18:42:21 +0200

    ⭐️ SEO

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <meta name="description" content="Sofia Fischer, Full Stack Code Wizard, Web and App Developer, and Agile Ethusiast"/>
+    <meta name="description" content="Sofia Fischer, Full Stack Code Wizard, Freelancer and always looking for the next
+                                        Challenge. Have a look at my projects and contact me for more information. "/>
+    <meta name="keywords" content="Sofia Fischer, Full Stack, Code Wizard, Developer, Freelancer, Philodev, Laravel, Web"/>
+    <meta name="author" content="Sofia Fischer"/>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Sofia Fischer | Philomathic Web Developer</title>
     <link rel="icon" type="image/png" href="{!! asset('storage/images/logo_black_circle.png') !!}"/>
 
     <!-- Fonts -->
